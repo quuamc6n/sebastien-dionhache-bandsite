c292c6299827b3c4c2e3b5e1cbb5c240b9498721
Sebastien <sebdh@hotmail.com>
2023-06-08 14:41:18 -0600

    linked appropriate scrip file and initialized array for comments section

---
+++ scripts/index-page.js
@@ -0,0 +1,19 @@
+
+let commentsArray = [
+  {
+    Name: "Connor Walton",
+    Timestamp: "02/17/2021",
+    Text: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
+  },
+  {
+    Name: "Emilie Beach",
+    Timestamp: "01/09/2021",
+    Text: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
+  },
+  {
+    Name: "Miles Acosta",
+    Timestamp: "12/20/2020",
+    Text: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
+  },
+];
+console.log(commentsArray);
