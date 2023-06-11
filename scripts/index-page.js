9c73a1f9a5b2d4c398297dc0d9e085bd5f3735d9
Sebastien <sebdh@hotmail.com>
2023-06-11 11:13:28 -0600

    fixed append in createcard to prepend contain div into main container to allow latest comments to show up first

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -16,7 +16,6 @@ let commentInformation = [
     Text: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
   },
 ];
-
 const mainContainer = document.querySelector(".comments__container");
 const formEvent = document.querySelector(".form");
 
@@ -30,7 +29,7 @@ function createCard (array) {
       // create main div that will hold text and avatar
       const container = document.createElement("div");
       container.className = "comments__container-main";
-      mainContainer.appendChild(container);
+      mainContainer.prepend(container);
       
       // create the avatar container and append into main div
       const avatarContainer = document.createElement("div");
@@ -41,7 +40,7 @@ function createCard (array) {
       const displayAvatar = document.createElement('img');
       displayAvatar.className = 'comments__container-main-avatar-img';
       avatarContainer.appendChild(displayAvatar);
-      displayAvatar.src = '';
+      displayAvatar.src = "";
 
       // create text div and append into main div
       const textContainer = document.createElement("div");
@@ -75,7 +74,7 @@ function createCard (array) {
       textContainer.appendChild(textPar);
     }
 }
-createCard(commentInformation);
+createCard(commentInformation.reverse());
 
 const formAvatar = document.querySelector('.form__img');
 function createFormAvatar (img){
@@ -96,17 +95,17 @@ function displayComment(comment){
     });
     const button = document.querySelector('.form__button');
     button.addEventListener('click', () => {
-        commentInformation.unshift({
+        comment.unshift({
           Name: getNewName(),
           Text: getNewComment(),
           Timestamp: getCurrentTime(),
         });
-        console.log(commentInformation[0]);
-        return commentInformation[0];
+        formEvent.reset();
+        createCard([comment[0]]);
     });
-    formEvent.reset();
 }
 displayComment(commentInformation);
+
 
 function getCurrentTime(){
     let today = new Date();
@@ -126,56 +125,8 @@ function getNewComment() {
     return newComment.value;
 }
 
-// function pushNewestComment (func) {
-//     console.log(func.Name);
-//     let name = func.Name;
-//     let text = func.Text;
-//     let date = func.Timestamp;
+// function pushNewestComment (obj) {
 
-//   // create main div that will hold text and avatar
-//   const container = document.createElement("div");
-//   container.className = "comments__container-main";
-//   mainContainer.prepend(container);
 
-//   // create the avatar container and append into main div
-//   const avatarContainer = document.createElement("div");
-//   avatarContainer.className = "comments__container-main-avatar";
-//   container.prepend(avatarContainer);
 
-//   // create image element to house avatar
-//   const displayAvatar = document.createElement("img");
-//   displayAvatar.className = "comments__container-main-avatar-img";
-//   avatarContainer.appendChild(displayAvatar);
-//   displayAvatar.src = "";
-
-//   // create text div and append into main div
-//   const textContainer = document.createElement("div");
-//   textContainer.className = "comments__container-main-text";
-//   container.appendChild(textContainer);
-
-//   // create div holding name and date for styling
-//   const nameDateDiv = document.createElement("div");
-//   nameDateDiv.className = "comments__container-main-name-date";
-//   textContainer.appendChild(nameDateDiv);
-
-//   // create p for name and prepend into nameDateDiv
-//   const namePar = document.createElement("p");
-//   namePar.className = "comments__container-main-text-name";
-//   nameText = document.createTextNode(name);
-//   namePar.appendChild(nameText);
-//   nameDateDiv.appendChild(namePar);
-
-//   // create p for date and append into nameDateDiv
-//   const datePar = document.createElement("p");
-//   datePar.className = "comments__container-main-text-date";
-//   dateText = document.createTextNode(date);
-//   datePar.appendChild(dateText);
-//   nameDateDiv.appendChild(datePar);
-
-//   // create p for text and append it to textContainer div
-//   const textPar = document.createElement("p");
-//   textPar.className = "comments__container-main-text-input";
-//   textInput = document.createTextNode(text);
-//   textPar.appendChild(textInput);
-//   textContainer.appendChild(textPar);
 // }
